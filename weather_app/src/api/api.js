3ffa9d7aad5c7ea4e5536389220a7d28ccb5eadd
PRAKHAR BARSAINYA <148670847+PrakharBarsainya@users.noreply.github.com>
2025-08-01 23:14:40 +0530

    Add files via upload

---
+++ weather_app/src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = '8ba5d68f3c3c6bac49bc23daf40ee2e5';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
